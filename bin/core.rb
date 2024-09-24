e1eaa60c283ca6cd7f8e75ac577c15c660075c5c
benzidane027 <benzidan27@yahoo.com>
2024-09-24 13:48:16 +0100

    up

--- bin/core.rb
+++ bin/core.rb
@@ -18,21 +18,21 @@ class Bot
         when '/help'
           @bot.api.send_message(chat_id: message.from.id, text: '/d example.video.mp4')
         when '/d'
-          begin
-            video_url = message.to_s.split.last
-            Tempfile.open(['downloaded_file', '.mp4']) do |temp_file|
-              @bot.api.send_message(chat_id: message.from.id, text: 'download in server ..')
-              video = URI.open(video_url)
-              @bot.api.send_message(chat_id: message.from.id, text: 'sending to client ..')
-              temp_file.binmode
-              temp_file.write(video.read)
-              temp_file.rewind
-              @bot.api.send_video(chat_id: message.from.id, video: Faraday::UploadIO.new(temp_file, 'video/mp4'))
-            end
-          rescue StandardError => e
-            puts e
-            @bot.api.send_message(chat_id: message.from.id, text: 'invalid input . try again..')
+          # begin
+          video_url = message.to_s.split.last
+          Tempfile.open(['downloaded_file', '.mp4']) do |temp_file|
+            @bot.api.send_message(chat_id: message.from.id, text: 'download in server ..')
+            video = URI.open(video_url)
+            @bot.api.send_message(chat_id: message.from.id, text: 'sending to client ..')
+            temp_file.binmode
+            temp_file.write(video.read)
+            temp_file.rewind
+            @bot.api.send_video(chat_id: message.from.id, video: Faraday::UploadIO.new(temp_file, 'video/mp4'))
           end
+          # rescue => e
+          #   puts e
+          #   @bot.api.send_message(chat_id: message.from.id, text: 'invalid input . try again..')
+          # end
 
         else
           @bot.api.send_message(chat_id: message.from.id, text: 'command not found')
